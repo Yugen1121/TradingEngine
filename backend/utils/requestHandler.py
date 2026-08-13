b10eec6d839bb63f039ed985a0fbf208e3d33bdf
Yugen1121 <yugenlimbu777@gmail.com>
2026-08-13 19:55:40 +0100

    converted it into async

--- backend/utils/requestHandler.py
+++ backend/utils/requestHandler.py
@@ -1,6 +1,7 @@
 from Models.model import RequestQueue, OrderBookManager
 from threading import Thread
 from datetime import datetime
+import asyncio
 import time
 class RequestHandler:
     def __init__(self, orderBook: OrderBookManager, requestQueue, logQueue):
@@ -10,23 +11,22 @@ class RequestHandler:
         self.running = True
         self.requests_processed = 0
 
-        self._thread = Thread(target=self.process_request, 
-                              daemon=True, 
-                              name="RequestProcessingThread")
+        self.task = None
     
-    def process_request(self):
-        
+    async def process_request(self):
         while self.running:
             order = self._request_queue.dequeue()
             if order:
-                self._order_book.submit(order)
+                await self._order_book.submit(order)
                 print(self._order_book.books[order._stock][order._type].inorder(self._order_book.books[order._stock][order._type].root))
                 self.requests_processed += 1
             else:
-                time.sleep(0.001)
+                await asyncio.sleep(0.001)
 
     def start(self):
-        self._thread.start()
+        self.task = asyncio.create_task(
+            self.process_request()
+        )
 
 
         
